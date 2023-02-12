c018aeac1a52fda8253b9e82995049b0414672d7
Gergo Horvath <111783205+horger89@users.noreply.github.com>
2023-02-12 20:19:11 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>src/App.js</code> hi there an by 2 3  ....
         </p>
         <a
           className="App-link"
